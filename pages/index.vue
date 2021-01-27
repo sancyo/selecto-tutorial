9871749a091885be271cd51380a88812166497ab
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2021-01-27 11:29:16 +0900

    add:選択できるコンテナを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,191 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <vue-selecto
+      drag-container=".elements"
+      :selectable-targets="['.selecto-area .cube']"
+      :hit-rate="10"
+      :select-by-click="true"
+      :select-from-inside="false"
+      :ratio="0"
+      @select="onSelect"
+    ></vue-selecto>
+    <div id="selecto1" class="elements selecto-area">
+      <div v-for="(cube, index) in cubes" :key="index" class="cube"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { VueSelecto } from 'vue-selecto'
+export default {
+  components: {
+    VueSelecto,
+  },
+  data() {
+    const cubes = []
+
+    for (let i = 0; i < 60; ++i) {
+      cubes.push(i)
+    }
+    return {
+      cubes,
+    }
+  },
+  methods: {
+    onSelect(e) {
+      console.log(e)
+      e.added.forEach((el) => {
+        console.log(el)
+        el.classList.add('selected')
+      })
+      e.removed.forEach((el) => {
+        console.log(el)
+        el.classList.remove('selected')
+      })
+    },
+  },
+}
 </script>
 
 <style>
+html,
+body,
+#root {
+  position: relative;
+  margin: 0;
+  padding: 0;
+  color: #333;
+  background: #fdfdfd;
+}
+
 .container {
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+}
+
+.app {
+  position: relative;
+  padding: 10px 20px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 800px;
+}
+
+body {
+  background: #fff;
+}
+
+.logo {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  margin: 0px auto;
+  font-size: 0;
+  text-align: left;
+}
+
+.logo.logos {
+  width: 320px;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
+.logos .selecto {
+  padding: 16px;
+}
+
+.logo img {
+  position: relative;
+  height: 100%;
+  box-sizing: border-box;
+}
+
+.cube {
+  display: inline-block;
+  border-radius: 5px;
+  width: 40px;
+  height: 40px;
+  margin: 4px;
+  background: #eee;
+  --color: #4af;
+}
+
+h1,
+.description {
+  text-align: center;
+}
+
+.button {
+  border: 1px solid #333;
+  color: #333;
+  background: transparent;
+  appearance: none;
+  -webkit-appearance: none;
+  box-sizing: border-box;
+  cursor: pointer;
+  width: 120px;
+  height: 42px;
+  font-size: 14px;
   letter-spacing: 1px;
+  transition: all ease 0.2s;
+  margin: 0px 5px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.button:hover {
+  background: #333;
+  color: white;
 }
 
-.links {
-  padding-top: 15px;
+.elements {
+  margin-top: 40px;
+  border: 2px solid #eee;
+}
+
+.selecto-area {
+  padding: 20px;
+}
+
+#selecto1 .cube {
+  transition: all ease 0.2s;
+}
+
+.moveable #selecto1 .cube {
+  transition: none;
+}
+
+.selecto-area .selected {
+  color: #fff;
+  background: var(--color);
+}
+
+.scroll {
+  overflow: auto;
+  padding-top: 10px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.infinite-viewer,
+.scroll {
+  width: 100%;
+  height: 300px;
+  box-sizing: border-box;
+}
+
+.infinite-viewer .viewport {
+  padding-top: 10px;
+}
+
+.empty.elements {
+  border: none;
 }
 </style>
